02ebaa55525ed22286102ed2229ab978e00677d8
AnaCarolinaFortunato <anacarolinafortunato@gmail.com>
2021-01-26 13:25:14 -0300

    feat: primeiros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+templateUrl:'.app.component.html'
+styleUrls: ['./app.component.scss']
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
